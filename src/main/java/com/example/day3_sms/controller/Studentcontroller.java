139b50b0a62740baae18c4e3a26111241758d1ef
SARA1326 <saransh1326.fzd@gmail.com>
2026-01-27 15:39:35 +0530

    done

--- src/main/java/com/example/day3_sms/controller/Studentcontroller.java
+++ src/main/java/com/example/day3_sms/controller/Studentcontroller.java
@@ -1,19 +1,36 @@
-package com.example.day3_sms.controller;
+package com.example.day3_project.controller;
 
-import com.example.day3_sms.model.Studentmodel;
-import com.example.day3_sms.service.StudentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.day3_project.model.StudentModel;
+import com.example.day3_project.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-public class Studentcontroller {
+public class StudentController {
+
     private final StudentService service;
-    public Studentcontroller(StudentService service){
+    public StudentController(StudentService service) {
         this.service = service;
     }
-    @PostMapping("add-student/")
-    public Studentmodel addstudent(@RequestBody Studentmodel student){
-        return service.addstudent(student);
+
+    @PostMapping("/add-student")
+    public StudentModel addStudent(@RequestBody StudentModel student) {
+
+        return service.addStudent(student);
+    }
+
+    @GetMapping("/students")
+    public List<StudentModel> getStudents(){
+        return service.getAllStudents();
+    }
+
+    @PutMapping("/update/{id}")
+    public StudentModel updateStudent(@PathVariable String id, @RequestBody StudentModel student) {
+        return service.updateStudent(id,student);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudent(@PathVariable String id) {
+        service.deleteStudent(id);
     }
 }
